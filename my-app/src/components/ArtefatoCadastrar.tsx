6ff7a7bd366cff2d77becb33461294258a877507
LohannaBieti <142019713+LohannaBieti@users.noreply.github.com>
2024-11-22 21:09:07 -0300

    Correção do artefatocadastrar

--- my-app/src/components/ArtefatoCadastrar.tsx
+++ my-app/src/components/ArtefatoCadastrar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Artefato } from "../interfaces/Artefato";
+import { Paleontologo } from "../interfaces/Paleontologo";
 import './ArtefatoCadastrar.css'
 
 function ArtefatoCadastrar() {
@@ -18,97 +19,53 @@ function ArtefatoCadastrar() {
         });
     });
   
-    function enviarProduto(e: any) {
+    function enviarArtefato(e: any) {
       e.preventDefault();
-  
+
       const artefato: Artefato = {
         nome: nome,
         periodo: periodo,
-        quantidade: Number(quantidade),
-        preco: Number(preco),
-        categoriaId: categoriaId,
+        civilizacaoDeOrigem: civilizacaoDeOrigem,
+        funcionalidade: funcionalidade,
+        dimensao:  dimensao,
+        material: material,
+        arqueologoId: arqueologoId,
+        adicionadoEm: adicionadoEm,
       };
-  
-      fetch("http://localhost:5020/api/artefato/cadastrar", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(artefato),
+
+      fetch("http://localhost:5020/api/artefato/listar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(artefato),
+    })
+      .then((resposta) => {
+        return resposta.json();
       })
-        .then((resposta) => {
-          return resposta.json();
-        })
-        .then((artefato) => {
-          console.log("Artefato cadastrado", artefato);
-        });
-    }
-  
-    return (
-      <div id="cadastrar_artefato" className="container">
-        <h1>Cadastrar Artefato</h1>
-        <form onSubmit={enviarArtefato}>
-          <div>
-            <label htmlFor="nome">Nome</label>
-            <input
-              type="text"
-              id="nome"
-              name="nome"
-              required
-              onChange={(e: any) => setNome(e.target.value)}
-            />
-          </div>
-  
-          <div>
-            <label htmlFor="periodo">Periodo</label>
-            <input
-              type="text"
-              id="periodo"
-              name="periodo"
-              onChange={(e: any) => setPeriodo(e.target.value)}
-            />
-          </div>
-  
-          <div>
-            <label htmlFor="preco">Preço</label>
-            <input
-              type="number"
-              id="preco"
-              name="preco"
-              onChange={(e: any) => setPreco(e.target.value)}
-            />
-          </div>
-  
-          <div>
-            <label htmlFor="quantidade">Quantidade</label>
-            <input
-              type="number"
-              id="quantidade"
-              name="quantidade"
-              onChange={(e: any) => setQuantidade(e.target.value)}
-            />
-          </div>
-  
-          <div>
-            <label htmlFor="quantidade">Categorias</label>
-            <select
-              onChange={(e: any) => setCategoriaId(e.target.value)}
-            >
-              {categorias.map((categoria) => (
-                <option
-                  value={categoria.categoriaId}
-                  key={categoria.categoriaId}
-                >
-                  {categoria.nome}
-                </option>
-              ))}
-            </select>
-          </div>
-  
-          <button type="submit">Cadastrar Artefato</button>
-        </form>
-      </div>
-    );
+      .then((produto) => {
+        console.log("Produto cadastrado", produto);
+      });
   }
-  
-  export default ArtefatoCadastrar;
+  return (
+    <div id="cadastrar_artefato" className="container">
+      <h1>Cadastrar Artefato</h1>
+      <form onSubmit={enviarArtefato}>
+        <div>
+          <label htmlFor="nome">Nome</label>
+          <input
+            type="text"
+            id="nome"
+            name="nome"
+            required
+            onChange={(e: any) => setNome(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">Cadastrar Artefato</button>
+      </form>
+    </div>
+  );
+}
+
+export default ArtefatoCadastrar;
